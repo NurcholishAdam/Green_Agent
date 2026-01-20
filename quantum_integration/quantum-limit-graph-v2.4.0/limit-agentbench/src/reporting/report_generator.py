cf1090ec13e225d26315eeb79ec5b26363039c26
Nurcholish Adam <nurcholisadam@gmail.com>
2026-01-20 17:39:15 +0700

    Create report_generator.py

---
+++ quantum_integration/quantum-limit-graph-v2.4.0/limit-agentbench/src/reporting/report_generator.py
@@ -0,0 +1,21 @@
+# src/reporting/report_generator.py
+
+class ReportGenerator:
+    def generate_full_report(self, results, scenario='production'):
+        """Generate three-layer report"""
+        reporter = LayeredReporter()
+        
+        reports = []
+        for result in results:
+            layer1 = reporter.generate_layer1(result)
+            layer2 = reporter.generate_layer2(result, result['complexity'])
+            layer3 = reporter.generate_layer3(result, scenario)
+            
+            reports.append({
+                'agent_id': result['agent_id'],
+                'layer1_raw': layer1.to_dict(),
+                'layer2_normalized': layer2.to_dict(),
+                'layer3_scenario': layer3.to_dict()
+            })
+        
+        return reports
