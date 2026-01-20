8040d370f0588c1b474242ae81581b78da9c3b3c
Nurcholish Adam <nurcholisadam@gmail.com>
2026-01-20 17:38:34 +0700

    Update and rename olicy_evaluator.py to policy_evaluator.py

--- quantum_integration/quantum-limit-graph-v2.4.0/limit-agentbench/src/rlhf/policy_evaluator.py
+++ quantum_integration/quantum-limit-graph-v2.4.0/limit-agentbench/src/rlhf/policy_evaluator.py
@@ -25,3 +25,18 @@ class PolicyEvaluationEnvironment:
             'mode': mode.value,
             'policy_performance': rewards
         }
+
+def compare_policies_across_modes(agent_policies, tasks):
+    """Compare policies across all execution modes"""
+    env = PolicyEvaluationEnvironment()
+    
+    results = {}
+    for policy_name, policy in agent_policies.items():
+        results[policy_name] = {}
+        
+        for mode in ExecutionMode:
+            if mode != ExecutionMode.CUSTOM:
+                eval_result = env.evaluate_policy(policy, tasks, mode)
+                results[policy_name][mode.value] = eval_result
+    
+    return results
