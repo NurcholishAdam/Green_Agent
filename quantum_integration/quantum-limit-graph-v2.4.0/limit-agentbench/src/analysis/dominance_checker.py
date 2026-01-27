294f7fa1b818b03b696821cf61fbaccc85d27683
Nurcholish Adam <nurcholisadam@gmail.com>
2026-01-27 22:46:01 +0700

    Update dominance_checker.py

--- quantum_integration/quantum-limit-graph-v2.4.0/limit-agentbench/src/analysis/dominance_checker.py
+++ quantum_integration/quantum-limit-graph-v2.4.0/limit-agentbench/src/analysis/dominance_checker.py
@@ -79,3 +79,23 @@ class DominanceChecker:
 
         return better_in_any
 
+
+# src/analysis/dominance_checker.py
+
+def dominates(a: dict, b: dict, minimize: set, maximize: set) -> bool:
+    better_or_equal = True
+    strictly_better = False
+
+    for k in minimize:
+        if a[k] > b[k]:
+            better_or_equal = False
+        elif a[k] < b[k]:
+            strictly_better = True
+
+    for k in maximize:
+        if a[k] < b[k]:
+            better_or_equal = False
+        elif a[k] > b[k]:
+            strictly_better = True
+
+    return better_or_equal and strictly_better
