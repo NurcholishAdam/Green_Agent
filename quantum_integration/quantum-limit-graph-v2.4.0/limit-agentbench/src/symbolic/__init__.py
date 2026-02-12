a425cf49bd0bcc084cae336a0bda2da0032e915b
Nurcholish Adam <nurcholisadam@gmail.com>
2026-02-12 19:50:30 +0700

    Create __init__.py

---
+++ quantum_integration/quantum-limit-graph-v2.4.0/limit-agentbench/src/symbolic/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Symbolic Reasoning Module
+
+Neuro-symbolic oversight for Green Agent inspired by FormalJudge paradigm.
+"""
+
+from .symbolic_reasoning_engine import SymbolicReasoningEngine, SymbolicRule, ViolationTrace
+
+__all__ = ['SymbolicReasoningEngine', 'SymbolicRule', 'ViolationTrace']
